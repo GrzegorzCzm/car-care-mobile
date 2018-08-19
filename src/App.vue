3dbfe0c6c66a197aabf2e8abf2b8cf7a279a7c0d
grzegorz.czmielewski <czmielewski@gmail.com>
2018-08-19 20:59:42 +0200

    added simple login page with firebase

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,35 @@
 <template>
   <!-- App -->
   <div id="app">
-    <RightSidebarMenu/>
+    <RightSidebarMenu/>    <f7-login-screen class="demo-login-screen" :opened="loginScreenOpened" @loginscreen:closed="loginScreenOpened = false">
+      <f7-page login-screen>
+        <f7-login-screen-title>CarCare login page</f7-login-screen-title>
+        <f7-list form>
+          <f7-list-item>
+            <f7-label>Username</f7-label>
+            <f7-input type="text" placeholder="Your username" @input="username = $event.target.value"></f7-input>
+          </f7-list-item>
+          <f7-list-item>
+            <f7-label>Password</f7-label>
+            <f7-input type="password" placeholder="Your password" @input="password = $event.target.value"></f7-input>
+          </f7-list-item>
+        </f7-list>
+        <f7-list>
+          <f7-list-button @click="signIn">Sign In</f7-list-button>
+          <f7-block-footer>Footer</f7-block-footer>
+        </f7-list>
+      </f7-page>
+    </f7-login-screen>
         <NavigationBar/>
         <f7-view id="main-view" url="/" main>
-        </f7-view>
-  </div>
+        </f7-view>  
+        
+        </div>
 </template>
 
 <script>
+import firebase from "firebase";
+
 import NavigationBar from "./assets/NavigationBar";
 import RightSidebarMenu from "./assets/RightSidebarMenu";
 
@@ -19,7 +40,11 @@ export default {
   store: Store,
 
   data() {
-    return {};
+    return {
+      loginScreenOpened: true,
+      username: "",
+      password: ""
+    };
   },
   components: {
     NavigationBar,
@@ -28,6 +53,25 @@ export default {
   computed: {
     isiOS() {
       return window.isiOS;
+    }
+  },
+  methods: {
+    signIn() {
+      const self = this;
+      const app = self.$f7;
+
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(self.username, self.password)
+        .then(
+          user => {
+            console.log("LOGGED IN USER:", user);
+            app.loginScreen.close();
+          },
+          error => {
+            app.dialog.alert(`Uppsss... wrong credentials`);
+          }
+        );
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -27,6 +27,18 @@ import store from './store/store'
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue, Framework7, Vuex);
 
+import firebase from "firebase";
+
+var config = {
+  apiKey: "AIzaSyAjtDfcFURJoSzs9G07QtcMjUzGe3x_3tU",
+  authDomain: "carcare-5fb92.firebaseapp.com",
+  databaseURL: "https://carcare-5fb92.firebaseio.com",
+  projectId: "carcare-5fb92",
+  storageBucket: "carcare-5fb92.appspot.com",
+  messagingSenderId: "706928502670"
+};
+firebase.initializeApp(config);
+
 // Init App
 const baseApp = new Vue({
   el: '#app',
